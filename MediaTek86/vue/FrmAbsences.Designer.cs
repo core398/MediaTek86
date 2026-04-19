83b526a946ac32c1b44b57e84985759b90bb6ef2
core <itszlayee@gmail.com>
2026-04-19 20:47:06 +0200

    Étape 2 - Interfaces visuelles FrmPrincipal et FrmAbsences

---
+++ MediaTek86/vue/FrmAbsences.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace MediaTek86.vue
+{
+    partial class FrmAbsences
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lvwAbsences = new System.Windows.Forms.ListView();
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.btnModifier = new System.Windows.Forms.Button();
+            this.btnSupprimer = new System.Windows.Forms.Button();
+            this.lblNomPrenom = new System.Windows.Forms.Label();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(154, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gestion des absences";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lvwAbsences
+            // 
+            this.lvwAbsences.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lvwAbsences.FullRowSelect = true;
+            this.lvwAbsences.GridLines = true;
+            this.lvwAbsences.HideSelection = false;
+            this.lvwAbsences.Location = new System.Drawing.Point(12, 60);
+            this.lvwAbsences.Name = "lvwAbsences";
+            this.lvwAbsences.Size = new System.Drawing.Size(515, 157);
+            this.lvwAbsences.TabIndex = 1;
+            this.lvwAbsences.UseCompatibleStateImageBehavior = false;
+            this.lvwAbsences.View = System.Windows.Forms.View.Details;
+            // 
+            // btnAjouter
+            // 
+            this.btnAjouter.Location = new System.Drawing.Point(123, 224);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(95, 35);
+            this.btnAjouter.TabIndex = 2;
+            this.btnAjouter.Text = "Ajouter";
+            this.btnAjouter.UseVisualStyleBackColor = true;
+            // 
+            // btnModifier
+            // 
+            this.btnModifier.Location = new System.Drawing.Point(224, 224);
+            this.btnModifier.Name = "btnModifier";
+            this.btnModifier.Size = new System.Drawing.Size(95, 35);
+            this.btnModifier.TabIndex = 3;
+            this.btnModifier.Text = "Modifier";
+            this.btnModifier.UseVisualStyleBackColor = true;
+            // 
+            // btnSupprimer
+            // 
+            this.btnSupprimer.Location = new System.Drawing.Point(325, 224);
+            this.btnSupprimer.Name = "btnSupprimer";
+            this.btnSupprimer.Size = new System.Drawing.Size(95, 35);
+            this.btnSupprimer.TabIndex = 4;
+            this.btnSupprimer.Text = "Supprimer";
+            this.btnSupprimer.UseVisualStyleBackColor = true;
+            // 
+            // lblNomPrenom
+            // 
+            this.lblNomPrenom.AutoSize = true;
+            this.lblNomPrenom.Location = new System.Drawing.Point(233, 33);
+            this.lblNomPrenom.Name = "lblNomPrenom";
+            this.lblNomPrenom.Size = new System.Drawing.Size(0, 13);
+            this.lblNomPrenom.TabIndex = 5;
+            this.lblNomPrenom.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Date début";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Date fin";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Motif";
+            // 
+            // FrmAbsences
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 270);
+            this.Controls.Add(this.lblNomPrenom);
+            this.Controls.Add(this.btnSupprimer);
+            this.Controls.Add(this.btnModifier);
+            this.Controls.Add(this.btnAjouter);
+            this.Controls.Add(this.lvwAbsences);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmAbsences";
+            this.Text = "Gestion des absences";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView lvwAbsences;
+        private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.Button btnModifier;
+        private System.Windows.Forms.Button btnSupprimer;
+        private System.Windows.Forms.Label lblNomPrenom;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
---
+++ MediaTek86/vue/frmPrincipal.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace MediaTek86.vue
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvwPersonnel = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.btnModifier = new System.Windows.Forms.Button();
+            this.btnSupprimer = new System.Windows.Forms.Button();
+            this.btnGererAbsences = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // lvwPersonnel
+            // 
+            this.lvwPersonnel.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.lvwPersonnel.FullRowSelect = true;
+            this.lvwPersonnel.GridLines = true;
+            this.lvwPersonnel.HideSelection = false;
+            this.lvwPersonnel.Location = new System.Drawing.Point(34, 49);
+            this.lvwPersonnel.Name = "lvwPersonnel";
+            this.lvwPersonnel.Size = new System.Drawing.Size(574, 224);
+            this.lvwPersonnel.TabIndex = 0;
+            this.lvwPersonnel.UseCompatibleStateImageBehavior = false;
+            this.lvwPersonnel.View = System.Windows.Forms.View.Details;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(225, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Liste du personnel";
+            // 
+            // btnAjouter
+            // 
+            this.btnAjouter.Location = new System.Drawing.Point(64, 284);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(90, 35);
+            this.btnAjouter.TabIndex = 2;
+            this.btnAjouter.Text = "Ajouter";
+            this.btnAjouter.UseVisualStyleBackColor = true;
+            // 
+            // btnModifier
+            // 
+            this.btnModifier.Location = new System.Drawing.Point(200, 284);
+            this.btnModifier.Name = "btnModifier";
+            this.btnModifier.Size = new System.Drawing.Size(90, 35);
+            this.btnModifier.TabIndex = 3;
+            this.btnModifier.Text = "Modifier";
+            this.btnModifier.UseVisualStyleBackColor = true;
+            // 
+            // btnSupprimer
+            // 
+            this.btnSupprimer.Location = new System.Drawing.Point(338, 284);
+            this.btnSupprimer.Name = "btnSupprimer";
+            this.btnSupprimer.Size = new System.Drawing.Size(90, 35);
+            this.btnSupprimer.TabIndex = 4;
+            this.btnSupprimer.Text = "Supprimer";
+            this.btnSupprimer.UseVisualStyleBackColor = true;
+            // 
+            // btnGererAbsences
+            // 
+            this.btnGererAbsences.Location = new System.Drawing.Point(469, 284);
+            this.btnGererAbsences.Name = "btnGererAbsences";
+            this.btnGererAbsences.Size = new System.Drawing.Size(90, 35);
+            this.btnGererAbsences.TabIndex = 5;
+            this.btnGererAbsences.Text = "Gérer les absences";
+            this.btnGererAbsences.UseVisualStyleBackColor = true;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Nom";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Prénom";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Téléphone";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Mail";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Service";
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 331);
+            this.Controls.Add(this.btnGererAbsences);
+            this.Controls.Add(this.btnSupprimer);
+            this.Controls.Add(this.btnModifier);
+            this.Controls.Add(this.btnAjouter);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lvwPersonnel);
+            this.Name = "frmPrincipal";
+            this.Text = "frmPrincipal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvwPersonnel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.Button btnModifier;
+        private System.Windows.Forms.Button btnSupprimer;
+        private System.Windows.Forms.Button btnGererAbsences;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+    }
+}
